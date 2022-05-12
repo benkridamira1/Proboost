b9ede3f4a5ec56f5a4ce37a9c103a99d016bd38f
Med-Marwen-BS <92266158+Med-Marwen-BS@users.noreply.github.com>
2022-05-12 20:35:21 +0100

    image add

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { HttpClient , HttpEventType } from '@angular/common/http';
 
 
 @Component({
@@ -10,6 +11,15 @@ import { FormGroup,FormControl,Validators } from '@angular/forms';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
+  //******** */
+
+  selectedFile !: File ;
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+  message !: string ;
+  imageName: any;
+  //*********** */
 
   user : any ;
   date !: any ;
@@ -23,7 +33,7 @@ export class ProfilComponent implements OnInit {
       date: new FormControl('',Validators.required),
     })
 
-  constructor(private authService : AuthenticationService,private router :Router) { 
+  constructor(private authService : AuthenticationService,private router :Router,private httpClient: HttpClient) { 
     
    }
 
@@ -59,5 +69,50 @@ export class ProfilComponent implements OnInit {
     return this.authService.updateUser(this.user).subscribe((data)=> console.log(data) );
     
   }
+
+  /******* */
+    //Gets called when the user selects an image
+    public onFileChanged(event: any) {
+      //Select File
+      this.selectedFile = event.target.files[0];
+    }
+  
+  
+    //Gets called when the user clicks on submit to upload the image
+    onUpload() {
+      console.log(this.selectedFile);
+      
+      //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+      const uploadImageData = new FormData();
+      uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+    
+      //Make a call to the Spring Boot Application to save the image
+      this.httpClient.post('http://localhost:3000/image/upload', uploadImageData, { observe: 'response' })
+        .subscribe((response) => {
+          if (response.status === 200) {
+            this.message = 'Image uploaded successfully';
+          } else {
+            this.message = 'Image not uploaded successfully';
+          }
+        }
+        );
+  
+  
+    }
+  
+      //Gets called when the user clicks on retieve image button to get the image from back end
+      getImage() {
+      //Make a call to Sprinf Boot to get the Image Bytes.
+      this.httpClient.get('http://localhost:3000/image/get/' + this.imageName)
+        .subscribe(
+          res => {
+            this.retrieveResonse = res;
+            this.base64Data = this.retrieveResonse.picByte;
+            this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+          }
+        );
+    }
+  
+  /******* */
 
 }
